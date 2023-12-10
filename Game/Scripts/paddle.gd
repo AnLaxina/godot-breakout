453a8cc368e588ba1ec91a548c0561f4bf800d75
Anilov Laxina <78817046+AnLaxina@users.noreply.github.com>
2023-12-10 07:39:09 -0800

    Apply movement to paddle

---
+++ Game/Scripts/paddle.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+
+@export var paddle_speed : int = 400
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	# Get the direction of the paddle based on the input that is pressed
+	var direction : int = Input.get_axis("move_left", "move_right")
+	# Adjust the built-in velocity in the x-direction based on the speed and direction
+	velocity.x = paddle_speed * direction
+	# Apply the velocity
+	move_and_slide()
+	
