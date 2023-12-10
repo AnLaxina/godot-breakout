1e7ed28dc551ab928c421b543c306377ca675fca
Anilov Laxina <78817046+AnLaxina@users.noreply.github.com>
2023-12-10 08:10:48 -0800

    Add ball & proper movement

---
+++ Game/Scripts/ball.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+
+func _ready():
+	# Set the initial speed of the ball
+	var speed = 600
+	# Set the initial direction of the ball to a random angle
+	var direction = Vector2.DOWN.rotated(randf_range(PI/3, PI/4))
+	# Set the initial velocity of the ball by multiplying the speed and direction
+	velocity = speed * direction
+		
+func _physics_process(delta):
+	# Returns the body that is hitting the ball
+	var collision_info = move_and_collide(velocity * delta)
+	# If something is hitting the ball bounce it back
+	if collision_info:
+		# Pushes the ball back in the opposite direction of the body that it hit
+		# A normal vector helps us achieve this by returning the oppposite direction
+		velocity = velocity.bounce(collision_info.get_normal())
--- Game/Scripts/paddle.gd
+++ Game/Scripts/paddle.gd
@@ -5,11 +5,11 @@ extends CharacterBody2D
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
+func _process(delta):
 	# Get the direction of the paddle based on the input that is pressed
 	var direction : int = Input.get_axis("move_left", "move_right")
 	# Adjust the built-in velocity in the x-direction based on the speed and direction
 	velocity.x = paddle_speed * direction
 	# Apply the velocity
-	move_and_slide()
+	move_and_collide(velocity * delta)
 	
