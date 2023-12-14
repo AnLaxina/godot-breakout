794a1a825fa1cd59597827b8d1eae16f8e8a91e4
Anilov Laxina <78817046+AnLaxina@users.noreply.github.com>
2023-12-13 19:02:56 -0800

    Add working functionality to brick deletion

--- Game/Scripts/ball.gd
+++ Game/Scripts/ball.gd
@@ -1,5 +1,9 @@
 extends CharacterBody2D
-
+# Initialize variables based on the colour of brick
+@onready var yellow_group = get_tree().get_nodes_in_group("yellow")
+@onready var green_group = get_tree().get_nodes_in_group("green")
+@onready var orange_group = get_tree().get_nodes_in_group("orange")
+@onready var red_group = get_tree().get_nodes_in_group("red")
 
 func _ready():
 	# Set the initial speed of the ball
@@ -8,12 +12,32 @@ func _ready():
 	var direction = Vector2.DOWN.rotated(randf_range(PI/3, PI/4))
 	# Set the initial velocity of the ball by multiplying the speed and direction
 	velocity = speed * direction
+	print(yellow_group)
+	
+	
 		
 func _physics_process(delta):
-	# Returns the body that is hitting the ball
+	# Can give us info on how the collision occurred (how it stopped, what body, etc.) 
 	var collision_info = move_and_collide(velocity * delta)
-	# If something is hitting the ball bounce it back
+	
+	# If something is hitting the ball, it is true
 	if collision_info:
 		# Pushes the ball back in the opposite direction of the body that it hit
 		# A normal vector helps us achieve this by returning the oppposite direction
 		velocity = velocity.bounce(collision_info.get_normal())
+		# Returns the body that the ball collided with
+		var collided_body = collision_info.get_collider()
+		
+		# Checks if the ball hit any yellow bricks
+		if collided_body in yellow_group:
+			collided_body.queue_free()
+		# Checks if ball hit any green bricks
+		if collided_body in green_group:
+			collided_body.queue_free()
+		# Checks if ball hit any orange bricks
+		if collided_body in orange_group:
+			collided_body.queue_free()
+		# Checks if ball hit any red bricks
+		if collided_body in red_group:
+			collided_body.queue_free()
+		
--- Game/Scripts/level.gd
+++ Game/Scripts/level.gd
@@ -1,6 +1,5 @@
 extends Node2D
 
-
 func _ready():
 	# Sets the background color to black once the game opens
 	# The reason why it's coded in is so that the color in the editor isn't black too.
