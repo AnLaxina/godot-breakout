b664782204635af8ee52643d8420bfd560ac4822
Anilov Laxina <78817046+AnLaxina@users.noreply.github.com>
2023-12-13 23:51:45 -0800

    Add working scoring system

--- Game/Scripts/level.gd
+++ Game/Scripts/level.gd
@@ -6,10 +6,14 @@ extends Node2D
 @onready var orange_group = get_tree().get_nodes_in_group("orange")
 @onready var red_group = get_tree().get_nodes_in_group("red")
 
+@onready var score_text = $ScoreText
+
 func _ready():
 	# Sets the background color to black once the game opens
 	# The reason why it's coded in is so that the color in the editor isn't black too.
 	RenderingServer.set_default_clear_color("black")
+	
+	score_text.text = "Score : " + str(PlayerData.player_score)
 	
 
 # A custom signal that stems from ball.gd
@@ -18,12 +22,19 @@ func _on_ball_on_broken_brick(body):
 	# Checks if the ball hit any yellow bricks
 		if body in yellow_group:
 			body.queue_free()
+			PlayerData.player_score += 1
 		# Checks if ball hit any green bricks
 		if body in green_group:
 			body.queue_free()
+			PlayerData.player_score += 3
 		# Checks if ball hit any orange bricks
 		if body in orange_group:
 			body.queue_free()
+			PlayerData.player_score += 5
 		# Checks if ball hit any red bricks
 		if body in red_group:
 			body.queue_free()
+			PlayerData.player_score += 7
+		
+		# Set player score on screen
+		score_text.text = "Score : " + str(PlayerData.player_score)
